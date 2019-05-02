89b589b5810ec210ea5f44d5b1db7a620f45f9ad
Neuroplant <m.roych@gmail.com>
2019-05-02 08:49:21 +0200

    Update LineFollowerTest.c

--- LineFollowerTest.c
+++ LineFollowerTest.c
@@ -4,14 +4,26 @@
 #include <stdio.h>
 
 #define LinePin0 0
+#define LinePin1 2
+
+int detectLine0;
+int detectLine1;
 
 void main (void) {
 	if(wiringPiSetup() == -1){ 
         printf("setup wiringPi faiservo !");
     };
 	pinMode(LinePin0, INPUT);
+	pinMode(LinePin1, INPUT);
 	
 	while(1) {
-		printf("LinePin %i\n", digitalRead(LinePin0));
+		detectLine0 = -1;
+		if (digitalRead(LinePin0)==LOW) then detectLine0 = 0;
+		if (digitalRead(LinePin0)==HIGH) then detectLine0 = 1;
+		printf("LinePin0 %i\n", detectLine0);
+		detectLine1 = -1;
+		if (digitalRead(LinePin1)==LOW) then detectLine1 = 0;
+		if (digitalRead(LinePin1)==HIGH) then detectLine1 = 1;
+		printf("LinePin1 %i\n", detectLine1);
 	}
 }
